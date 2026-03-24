6ef945f7236b151e29182d8d6402436c58b07b64
TwelveStudent47 <laczkokevin60@gmail.com>
2026-03-24 09:39:28 +0100

    feat: add authentication middleware to dashboard route

--- routes/main.js
+++ routes/main.js
@@ -3,12 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 const { login, dashboard } = require("../controllers/main");
+const authMiddleware = require("../middleware/auth");
 
 router.get("/health", (req, res) => {
     return res.status(200).json({ msg: "The API is healthy!" });
 })
 
-router.get("/dashboard", dashboard);
+router.get("/dashboard", authMiddleware, dashboard);
 
 router.post("/login", login);
 
